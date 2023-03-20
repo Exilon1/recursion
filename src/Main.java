b7d19c4ed37726b9b20b1aa91d7ec60df9280579
Aleksey Nikotin <exilonone@gmail.com>
2023-03-20 14:46:34 +0400

    palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("exponent = " + exponentiation(2, 3));
         System.out.println("sumOfDigits = " + sumOfDigits(123));
+
+        System.out.println("Sator arepo tenet opera rotas >> "
+                + validPalindrome("sator arepo tenet opera rotas"));
+        System.out.println("Sator arepo >> "
+                + validPalindrome("Sator arepo"));
+        System.out.println("Ss >> "
+                + validPalindrome("Ss"));
+        System.out.println(" a >> "
+                + validPalindrome(" a"));
+        System.out.println("S >> "
+                + validPalindrome("S"));
+
     }
 
     public static int exponentiation(int number, int exponent) {
@@ -26,5 +42,33 @@ public class Main {
         }
 
         return number % 10 + sumOfDigits(number/10);
+    }
+
+    public static boolean validPalindrome(String s) {
+        String string = s.toLowerCase();
+
+        LinkedList<Character> deque = string.chars().mapToObj(
+                c -> (char) c).collect(Collectors.toCollection(LinkedList::new)
+        );
+
+        if (s.isBlank()) {
+            return true;
+        }
+
+        Character left;
+        try {
+            left = deque.removeFirst();
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+
+        Character right;
+        try {
+            right = deque.removeLast();
+        } catch (NoSuchElementException e) {
+            return true;
+        }
+
+        return left.equals(right) && validPalindrome(s.substring(1, s.length()-1));
     }
 }
